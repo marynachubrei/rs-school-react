c5c7b02fde10f7f1dd448ac2c347bb91f0f993a5
marynachubrei <78369312+marynachubrei@users.noreply.github.com>
2025-07-12 00:29:14 +0300

    refactor: removed unused code

--- rs-react-app/src/services/products-api.ts
+++ rs-react-app/src/services/products-api.ts
@@ -1,11 +1,5 @@
 const BASE_URL = 'https://dummyjson.com/products';
 
-// export const getAllProducts = async () => {
-//   const response = await fetch(`${BASE_URL}`, {});
-//   const data = await response.json();
-//   return data.products;
-// };
-
 export const getAllProducts = async (query: string) => {
   const response = await fetch(
     `${BASE_URL}/search?q=${encodeURIComponent(query)}`,
